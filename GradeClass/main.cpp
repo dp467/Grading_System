4c256b407349c4715c8a37ae70f66406adf2299a
dp467 <47339298+dp467@users.noreply.github.com>
2020-04-07 23:42:04 -0400

    Update main.cpp

--- GradeClass/main.cpp
+++ GradeClass/main.cpp
@@ -7,9 +7,95 @@
 //
 
 #include <iostream>
+#include "Grade.h"
+
+
+void dispay();
+char option();
+
+
+
+void display(){
+    std::cout<< "Selected on of the option below : " << std::endl;
+    
+    std::cout << "A. First Quiz Grade " << std::endl;
+    
+    std::cout <<" B: Second Quiz Grade " << std::endl;
+    
+    std::cout <<"C: Midterm Exam Grade " << std::endl;
+   
+    std::cout <<"D: Final Exam Grade " << std::endl;
+    
+    std::cout << "Q : quit " << std::endl;
+  
+}
+char option(){
+    char input;
+    std:: cout<< "option : " << std::endl;
+    std::cin >> input;
+    return toupper(input);
+    
+    
+}
+
+
+
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    char options;
+     Grade mygrade;
+    do{
+        
+        display();
+        options = option();
+       
+        switch(options){
+            case 'A':{
+                int g{0};
+                std::cout << "Enter the grade of the first quiz out of 10 : "<< std:: endl;
+                std::cin >> g;
+                mygrade.get_quizgrade_1(g);
+                break;
+            }
+        
+            case 'B' :{
+               
+                int g{0};
+                std::cout << "Enter the grade of the first quiz out of 10 : "<< std:: endl;
+                std::cin >> g;
+                mygrade.get_quizgrade_2(g);
+                
+                break;
+            
+            }
+            case 'C' :{
+                int g{0};
+                std::cout << "Enter the grade of the midterm out of 100 : "<< std:: endl;
+                std::cin >> g;
+                mygrade.get_midtermgrade(g);
+                break;
+            }
+            case 'D' :{
+                int g{0};
+                std::cout << "Enter the grade of the final out of 100 : "<< std:: endl;
+                std::cin >> g;
+                mygrade.get_finalgrade(g);
+                break;
+                }
+        }
+        
+        
+        
+        
+        
+    }while (options != 'Q');{
+        std::cout<< "thanks for playing" << std::endl;
+    }
+    mygrade.get_total();
+    mygrade.letter_grade();
+
+    
     return 0;
 }
