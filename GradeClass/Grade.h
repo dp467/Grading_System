48b3db29c1f4bc9e8d500f2a3f43f1e5ff0e8966
dp467 <47339298+dp467@users.noreply.github.com>
2020-04-07 23:42:27 -0400

    Update Grade.h

--- GradeClass/Grade.h
+++ GradeClass/Grade.h
@@ -9,5 +9,23 @@
 #ifndef Grade_h
 #define Grade_h
 
+class Grade{
+private:
+    int quiz_grade_1;
+    int quiz_grade_2;
+    int midterm_grade;
+    int final_grade;
+    double total_grade;
+    
+    
 
+public:
+    void get_quizgrade_1 ( int grade);
+    void get_quizgrade_2 (int grade);
+    void get_midtermgrade (int grade);
+    void get_finalgrade (int grade);
+    void get_total();
+    void letter_grade();
+    
+};
 #endif /* Grade_h */
